b8c5e795b43d52229d7ef325ad6282c0dc69be79
Lara-fil <lara.isabel.cos@gmail.com>
2026-04-17 19:57:59 +0100

    Refactor HTML structure and improve semantics

--- theme.php
+++ theme.php
@@ -2,30 +2,25 @@
 <!DOCTYPE html>
 <html lang="<?= $Wcms->get('config', 'siteLang') ?>">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
 
-<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
-
-<?= $Wcms->css() ?>
-<link rel="stylesheet" href="<?= $Wcms->asset('style.css') ?>">
-
+    <?= $Wcms->css() ?>
+    <link rel="stylesheet" href="<?= $Wcms->asset('style.css') ?>">
 </head>
 <body>
 
 <?= $Wcms->settings() ?>
 <?= $Wcms->alerts() ?>
 
-<header>
+<header class="site-header">
     <a class="site-logo" href="<?= $Wcms->url() ?>">
         <?= $Wcms->get('config', 'siteTitle') ?>
     </a>
 
-    <nav>
-        <ul>
-            <?= $Wcms->menu() ?>
-        </ul>
+    <nav class="site-nav">
+        <?= $Wcms->menu() ?>
     </nav>
 </header>
 
@@ -38,6 +33,5 @@
 </footer>
 
 <?= $Wcms->js() ?>
-
 </body>
 </html>
